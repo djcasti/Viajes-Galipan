81ce7a2bdc10c9a7fd8172d3feaf0c3617736d78
Dennis Castillo <djcasti@gmail.com>
2017-03-15 22:11:10 -0400

    Se programa la carga dinámica de los campos país, estado y ciudad

--- Galipan.php
+++ Galipan.php
@@ -4,7 +4,17 @@
 	class MainGalipan extends DBConnector {
 
 		public function getPaises() {
-			$query = $this->queryList("SELECT * FROM PAIS", []);	
+			$query = $this->queryList("SELECT id_pais, nombre FROM pais WHERE codigo IN ('ES','VE')", []);	
+			return $query->fetchAll(PDO::FETCH_ASSOC);			
+		}
+
+		public function getEstados($idPais) {
+			$query = $this->queryList("SELECT id_estado, nombre FROM estado WHERE id_pais = :idPais", [':idPais' => $idPais]);	
+			return $query->fetchAll(PDO::FETCH_ASSOC);			
+		}
+
+		public function getCiudades($idEstado) {
+			$query = $this->queryList("SELECT id_ciudad, nombre FROM ciudad WHERE id_estado = :idEstado", [':idEstado' => $idEstado]);
 			return $query->fetchAll(PDO::FETCH_ASSOC);			
 		}
 	}
@@ -16,15 +26,23 @@
 		
 		switch ($_GET['option']) {
 			case 'getPaises':
-				$result[] = $db->getPaises();
+				$result = $db->getPaises();
 				break;
-			
+
+			case 'getEstados':
+				$result = $db->getEstados($_GET['idPais']);
+				break;
+
+			case 'getCiudades':
+				$result = $db->getCiudades($_GET['idEstado']);
+				break;
+
 			default:
 				# code...
 				break;
 		}
 	}
 	
-	echo json_encode($result);
+	echo json_encode($result, JSON_FORCE_OBJECT);
 		
 ?>
--- scripts/galipan.js
+++ scripts/galipan.js
@@ -2,11 +2,60 @@
 		var getPaises = function() {
 			$.ajax({
 				url: "Galipan.php",
+				dataType: 'json',
 				data: { "option": "getPaises" }
 			}).done(function(result) {
-				console.log(result);
+				var select = $("select[id*='Country']");
+				select.append($('<option />').text('Seleccione...'));
+
+				$.each(result, function(i, pais){
+					select.append($('<option />').text(pais.nombre).val(pais.id_pais));
+				});
 			});
 		};
 
+		$("select[id*='Country']").on('change', function() {
+			$.ajax({
+				url: "Galipan.php",
+				dataType: 'json',
+				data: { "option": "getEstados", 'idPais': $(this).val() },
+				context:  $(this)
+			}).done(function(result) {
+				var objeto = ($(this).attr('id')).slice(0, ($(this).attr('id')).indexOf("Country"));
+				var input = $('#'+ objeto + 'State');
+				
+				input.empty();
+				input.append($('<option />').text('Seleccione...'));
+				
+				$.each(result, function(i, estado){
+					input.append($('<option />').text(estado.nombre).val(estado.id_estado));
+				});
+			});
+		});
+
+		$("select[id*='State']").on('change', function() {
+			console.log($(this).val());
+			$.ajax({
+				url: "Galipan.php",
+				dataType: 'json',
+				data: { "option": "getCiudades", 'idEstado': $(this).val() },
+				context:  $(this)
+			}).done(function(result) {
+				console.log(result);
+				var objeto = ($(this).attr('id')).slice(0, ($(this).attr('id')).indexOf("State"));
+				var input = $('#'+ objeto + 'City');
+				
+				input.empty();
+				input.append($('<option />').text('Seleccione...'));
+				
+				$.each(result, function(i, ciudad){
+					input.append($('<option />').text(ciudad.nombre).val(ciudad.id_ciudad));
+				});
+			});
+		});
+
 		getPaises();
 	});
+
+	
+	
